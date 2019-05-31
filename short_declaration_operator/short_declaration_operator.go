d5aae791110c3bb0d6135f94b0650bfb21a5c019
Paul Gureghian <paulgureghian@att.net>
2019-05-31 13:26:51 -0700

    First commit

---
+++ short_declaration_operator/short_declaration_operator.go
@@ -0,0 +1,19 @@
+// Created by Paul A. Gureghian in June 2019
+// A Go source file which demonstrates the "short_declaration_operator"
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+
+	x = 99
+	fmt.Println(x)
+
+	y := 100 + 70
+	fmt.Println(y)
+
+	z := "Bond, James"
+	fmt.Println(z)
+}
