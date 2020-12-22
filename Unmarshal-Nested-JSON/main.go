bf1a90f6e68a17aa58854eb8d2ed0e5eb89fdec0
paulgureghian <paulgureghian@att.net>
2020-12-21 18:13:40 -0800

    Added main.go

---
+++ Unmarshal-Nested-JSON/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+const jsonText = `
+{
+ "librairies": [
+	{
+	 "name": "Paul Gureghian",
+	 "city": "los Angeles",
+	 "country": "USA",	
+	 "books": [
+		{
+		 "title": "Harry Potter",
+		 "author": "J. K. Rowling",
+		 "available": true			  
+		}
+	  ]
+	},
+	{
+	 "name": "Libreria Lowcost",
+	 "city": "Pamploma",
+	 "country": "Spain",
+	 "books": [
+		{
+		 "title": "The Subtle art of Caring",
+		 "author": "Mark Manson",
+		 "available": false		
+		}
+	   ]
+	  }
+    ]
+}`
+
+// Libraries is exported
+type Libraries struct {
+	LibrariesList []struct {
+		Name    string `json:"name"`
+		City    string `json:"city"`
+		Country string `json:"country"`
+		Books   []struct {
+			Title     string `json:"title"`
+			Author    string `json:"author"`
+			Available bool   `json:"available"`
+		} `json:"books"`
+	} `json:"libraries"`
+}
+
+func main() {
+	var librariesInformation Libraries
+	err := json.Unmarshal([]byte(jsonText), &librariesInformation)
+	if err != nil {
+		log.Fatal("Error Unmarshaling json: ", err)
+	}
+	println(" ")
+	log.Printf("librariesInformation: %+v", librariesInformation)
+	println(" ")
+}
