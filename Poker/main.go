81fd7c3651b60f39d34dca7338771e6760ec65a6
paulgureghian <paulgureghian@att.net>
2021-09-14 20:11:16 -0700

    Modified main.go source code file

--- Poker/main.go
+++ Poker/main.go
@@ -1,6 +1,6 @@
-// Created by Paul A. Gureghian in Sept 2021
+// Created by Paul A. Gureghian in Sept 2021.
 
-// A game of poker wriiten in Go
+// A game of Poker written in Go.
 
 package main
 
@@ -9,36 +9,46 @@ import (
 	"sort"
 )
 
+// Define function.
 func isHandOfStraights(hand []int, k int) bool {
 
+	// Check if hand is divisable by k.
 	if len(hand)%k != 0 {
 		return false
 	}
 
+	// Count the occurance of each card in the hand.
 	count := make(map[int]int)
 	for _, i := range hand {
 		count[i] = count[i] + 1
 	}
 
+	// Sort the list.
 	sort.Ints(hand)
 	i := 0
 	n := len(hand)
 
+	// Nested loop that runs k times.
 	for i < n {
 		current := hand[i]
 		for j := 0; j < k; j++ {
+			
+			// Check if the current card and the next k-1 cards are in the count map.
 			if _, ok := count[current+j]; !ok || count[current+j] == 0 {
 				return false
 			}
 
+			// When a card is in the count map, decrement it.
 			count[current+j]--
 		}
 
+		// Find the next group's smallest card.
 		for i < n && count[hand[i]] == 0 {
 			i++
 		}
 	}
 
+	// If all cards are sorted into groups.
 	return true
 }
 
