bd55e6a9b77bf7b2b4ae495cdbeeb78c150c050f
paulgureghian <paulgureghian@att.net>
2021-09-29 17:55:50 -0700

    Modifed file

--- Port_Scanner/main.go
+++ Port_Scanner/main.go
@@ -1,3 +1,7 @@
+// Created by Paul A. Gureghian in Sept 2021.
+
+// A simple port scanner written in Go.
+
 package main
 
 import (
@@ -10,7 +14,7 @@ import (
 func main() {
 
 	fmt.Println("Checking for available ports...")
-	ports := portscan.PortScan(os.Args[1])
+	ports := portscan.PortScan(os.Args [1])
 
 	fmt.Println("Ports available: ", ports)
 
